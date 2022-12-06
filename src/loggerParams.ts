3e8c01850b69ed83e3a87b9a1b228603afe9ae99
Pascal Zwikirsch <pascalrene.zwikirsch@gmail.com>
2022-12-06 20:07:46 +0100

    Initial

--- src/loggerParams.ts
+++ src/loggerParams.ts
@@ -1,4 +1,8 @@
 
 export interface LoggerParams {
-    // not yet used
+    /*
+        If set to `true` the message of a 'error' log will be wrapped
+        in a stack trace to be recognized by GCP's Error Reporting
+    */
+    GCP_ERROR_REPORTING?: boolean;
 }
--- src/logging.service.spec.ts
+++ src/logging.service.spec.ts
@@ -1,5 +1,9 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {LoggingService} from './logging.service';
+import {Severity} from './severity';
+
+const logMessage = 'my message';
+const logSpy = jest.spyOn(console, 'log');
 
 describe('LoggingService', () => {
   let service: LoggingService;
@@ -15,4 +19,79 @@ describe('LoggingService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('log', () => {
+    const severity = Severity.INFO;
+    it(`Should log with ${severity} severity`, () => {
+      service.log(logMessage);
+      expect(logSpy).toHaveBeenLastCalledWith(`{"severity":"${severity}","message":"${logMessage}"}`);
+    });
+  });
+
+  describe('error', () => {
+    const severity = Severity.ERROR;
+    it(`Should log with ${severity} severity`, () => {
+      service.error(logMessage);
+      expect(logSpy).toHaveBeenLastCalledWith(`{"severity":"${severity}","message":"${logMessage}"}`);
+    });
+  });
+
+  describe('warn', () => {
+    const severity = Severity.WARNING;
+    it(`Should log with ${severity} severity`, () => {
+      service.warn(logMessage);
+      expect(logSpy).toHaveBeenLastCalledWith(`{"severity":"${severity}","message":"${logMessage}"}`);
+    });
+  });
+
+  describe('debug', () => {
+    const severity = Severity.DEBUG;
+    it(`Should log with ${severity} severity`, () => {
+      service.debug(logMessage);
+      expect(logSpy).toHaveBeenLastCalledWith(`{"severity":"${severity}","message":"${logMessage}"}`);
+    });
+  });
+
+  describe('verbose', () => {
+    const severity = Severity.INFO;
+    it(`Should log with ${severity} severity`, () => {
+      service.verbose(logMessage);
+      expect(logSpy).toHaveBeenLastCalledWith(`{"severity":"${severity}","message":"${logMessage}"}`);
+    });
+  });
 });
+
+
+describe(`LoggingService with GCP_ERROR_REPORTING=true`, () => {
+  let service: LoggingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LoggingService,
+        {
+          provide: 'LoggingServiceParams', // this can be a symbol or a string
+          useValue: {GCP_ERROR_REPORTING: true},
+        },
+      ],
+    }).compile();
+
+    service = module.get<LoggingService>(LoggingService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('error', () => {
+    const severity = Severity.ERROR;
+    it(`Should log with ${severity} severity and stack trace in message`, () => {
+      service.error(logMessage);
+      // No need for checking the WHOLE stackstrace
+      const expectedLog = expect.stringContaining(`{"severity":"${severity}","message":"Error: ${logMessage}\\n    at LoggingService.error`);
+
+      expect(logSpy).toHaveBeenLastCalledWith(expectedLog);
+    });
+  });
+});
+
+
