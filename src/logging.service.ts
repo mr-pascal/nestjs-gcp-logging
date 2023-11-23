76af968fc9bb937cf6aa3c6fad517c476491fb3d
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2023-11-23 12:03:20 +0100

    refactor: extract composing log entry to protected method

--- src/logging.service.ts
+++ src/logging.service.ts
@@ -21,12 +21,20 @@ export class LoggingService extends ConsoleLogger implements LoggerService {
     super();
   }
 
+  protected composeEntry(severity: Severity, message: any) {
+    return { severity, message };
+  }
+
+  protected print(entry: object) {
+    console.log(JSON.stringify(entry));
+  }
+
   /**
    * Write an "INFO" level log
    * @param {any} message
    */
   log(message: any) {
-    console.log(JSON.stringify({severity: Severity.INFO, message: message}));
+    this.print(this.composeEntry(Severity.INFO, message));
   }
 
   /**
@@ -41,7 +49,7 @@ export class LoggingService extends ConsoleLogger implements LoggerService {
       message = new Error(composedMessage).stack;
     }
 
-    console.log(JSON.stringify({ severity: Severity.ERROR, message }));
+    this.print(this.composeEntry(Severity.ERROR, message));
   }
 
   /**
@@ -49,7 +57,7 @@ export class LoggingService extends ConsoleLogger implements LoggerService {
    * @param {any} message
    */
   warn(message: any) {
-    console.log(JSON.stringify({severity: Severity.WARNING, message: message}));
+    this.print(this.composeEntry(Severity.WARNING, message));
   }
 
   /**
@@ -57,7 +65,7 @@ export class LoggingService extends ConsoleLogger implements LoggerService {
    * @param {any} message
    */
   debug(message: any) {
-    console.log(JSON.stringify({severity: Severity.DEBUG, message: message}));
+    this.print(this.composeEntry(Severity.DEBUG, message));
   }
 
 
@@ -66,6 +74,6 @@ export class LoggingService extends ConsoleLogger implements LoggerService {
    * @param {any} message
    */
   verbose(message: any) {
-    console.log(JSON.stringify({severity: Severity.INFO, message: message}));
+    this.print(this.composeEntry(Severity.INFO, message));
   }
 }
